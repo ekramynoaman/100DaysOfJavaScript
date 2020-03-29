17564f2e7900cb7e4b6452cd2cf1a80c2eba7b4f
EkramyNo3man <Ekramy.no3man@gmail.com>
2020-03-30 01:17:19 +0200

    Prevent Duplicating

--- project_change_color_background/js/script.js
+++ project_change_color_background/js/script.js
@@ -1,14 +1,38 @@
 //Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+body.style.backgroundColor = 'violet';
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
 
-function changeBackground(){
-// Combine random number with index of colors array
-const colorIndex= parseInt(Math.random()*colors.length+1) // parseInt to convert any float number to integar
+button.addEventListener('click', changeBackground);
+
 // Change bg-color
-body.style.backgroundColor = colors[colorIndex]
+function changeBackground(){
+    // // Combine random number with index of colors array
+    // ParseInt to convert any float number to integar
+    let colorIndex= parseInt(Math.random()*colors.length)
+    console.log(colorIndex); // For test
+    console.log(colors[colorIndex]); // For test
+    
+    // Check if its the same previous color
+    if (body.style.backgroundColor == colors[colorIndex]) {
+        console.log(colorIndex); // For test
+        body.style.backgroundColor = colors[colorIndex+1] 
+        console.log('Changed to ' + body.style.backgroundColor  + ' index is ' + (colorIndex+1)); // For test
+        // Check if colorIndex >= array length
+        if (colorIndex+1 >= colors.length) {
+            body.style.backgroundColor = colors[colorIndex-1]
+            console.log(body.style.backgroundColor +' fix over'); // For test
+            
+        }
+
+    }
+    else 
+    {
+        body.style.backgroundColor = colors[colorIndex]
+
+    }
+
 }
+
